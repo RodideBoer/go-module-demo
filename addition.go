5f82f41d6df4ab75cee542d0986109f8fa64b4f6
Rodi de Boer <rodi@procurios.nl>
2026-07-24 21:16:54 +0200

    Make addition generic

--- addition.go
+++ addition.go
@@ -1,11 +1,17 @@
 // Package addition is used for the addition of two numbers.
 package addition
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add adds two numbers and returns the result.
 //
 // More information about addition can be found at [Math is fun].
 //
 // [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
