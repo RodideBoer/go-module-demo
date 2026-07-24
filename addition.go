244a2934a584f67ecc020b26d1128ff6bd12cd27
Rodi de Boer <rodi@procurios.nl>
2026-07-24 16:36:26 +0200

    Add godoc documentation

--- addition.go
+++ addition.go
@@ -1,5 +1,11 @@
+// Package addition is used for the addition of two numbers.
 package addition
 
+// Add adds two numbers and returns the result.
+//
+// More information about addition can be found at [Math is fun].
+//
+// [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
