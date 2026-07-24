10edf503fcff7b21cb0b3641c8123be36300c275
Rodi de Boer <rodi@procurios.nl>
2026-07-24 21:25:39 +0200

    Revert "Make addition generic"
    This reverts commit 5f82f41d6df4ab75cee542d0986109f8fa64b4f6.

--- addition.go
+++ addition.go
@@ -1,17 +1,11 @@
 // Package addition is used for the addition of two numbers.
 package addition
 
-import "golang.org/x/exp/constraints"
-
-type Number interface {
-	constraints.Integer | constraints.Float
-}
-
 // Add adds two numbers and returns the result.
 //
 // More information about addition can be found at [Math is fun].
 //
 // [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
-func Add[T Number](x, y T) T {
+func Add(x, y int) int {
 	return x + y
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/RodideBoer/go-module-demo
 
 go 1.26.4
-
-require golang.org/x/exp v0.0.0-20260718201538-764159d718ef
